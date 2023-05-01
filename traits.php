b34aa0e70242dc3826333fa6e1c20adc90bfbb72
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-05-01 12:31:26 +0800

    Traits class practice

---
+++ traits.php
@@ -0,0 +1,18 @@
+<?php
+
+trait Message1{
+  public function msg1(){
+    echo "Message 1 from trait class";
+    echo "<br>";
+  }
+}
+
+trait Message2{
+  public function msg2(){
+    echo "Message 2 from trait class";
+    echo "<br>";
+  }
+}
+
+
+?>
