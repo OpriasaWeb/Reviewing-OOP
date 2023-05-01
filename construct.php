dfff820b8bec035bec8f72a98737ff37fa59397c
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-05-01 10:40:13 +0800

    Construct OOP done reviewing.

---
+++ construct.php
@@ -0,0 +1,29 @@
+<?php
+
+class Car{
+  public $brand;
+
+  // Constructor
+  function __construct($brand)
+  {
+    $this->brand = $brand;
+  }
+
+  // Desctructor
+  function __destruct()
+  {
+    echo "Your car brand is {$this->brand}.";
+  }
+
+  function get_name(){
+    return $this->brand;
+  }
+
+}
+
+
+
+
+
+
+?>
